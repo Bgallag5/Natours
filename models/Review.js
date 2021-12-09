2f46c054e220ae4e7561d6dd4b4dd8053bcf526c
bengallagher <bgallag5@gmail.com>
2021-12-09 10:32:38 -0500

    post middleware

--- models/Review.js
+++ models/Review.js
@@ -68,19 +68,52 @@ reviewSchema.statics.calcAverageRating = async function (tourId) {
     },
   ]);
   console.log(stats);
+  //check if the tour has any ratings; only need to find and update if we have stats to add
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(
+      tourId,
+      {
+        ratingsAverage: stats[0].avgRating,
+        ratingsQuantity: stats[0].numRating,
+      },
+      { new: true, runValidators: true }
+    );
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsAverage: 4.5,
+      ratingsQuantity: 0,
+    });
+  }
   // update tour with new numRating and avgRating
-  await Tour.findByIdAndUpdate(
-    tourId,
-    { ratingsAverage: stats[0].avgRating, ratingsQuantity: stats[0].numRating },
-    { new: true, runValidators: true }
-  );
 };
 
 reviewSchema.post('save', function () {
   //'this' points to document being saved
-  // to access "Review" model before save, use this.constructor; this.constructor = Model {Review}
+  // to access "Review" model post save, use this.constructor; this.constructor = Model {Review}
   this.constructor.calcAverageRating(this.tour);
 });
+
+//findByIdAndUpdate
+//findByIdAndDelete
+//////////////////////////////////////////////////////////////////////
+// to be able to run calcAverageRatings on UPDATE AND DELETE we must use this QUERY middleware, not document middleware...
+// where we do not have access to the current document, instead we set this.doc in pre, then access this.doc in next post middleware 
+//these two middlewares work together to update a review and then re calculate average
+// 'this' keyword in query middleware is the current query
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  //mongoose 6.0 use .clone to re-execute a query
+  //set 'this.doc' to our current document
+  this.doc = await this.findOne().clone();
+  console.log(this.doc);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); - doesnt work in post becuase the query has already been executed
+  //use constructor to access 'Review' model
+  await this.doc.constructor.calcAverageRating(this.doc.tour);
+});
+//////////////////////////////////////////////////////////////////////
 
 const Review = model('Review', reviewSchema);
 
