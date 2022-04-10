11d559d76e11475806921aa237507141aa6128bf
bengallagher <bgallag5@gmail.com>
2022-04-10 15:42:14 -0400

    comment index in app

--- app.js
+++ app.js
@@ -98,6 +98,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.all('*', (req, res, next) => {
   // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
