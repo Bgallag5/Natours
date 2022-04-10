e87f43222b949cdeb7f77fcc682e3aa2cfee507a
bengallagher <bgallag5@gmail.com>
2022-04-10 15:53:17 -0400

    static

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
-const express = require('express');
 
 // serve up react front-end in production
 router.use((req, res) => {
-  res.sendFile(express.static(path.join(__dirname, '../client/build/index.html')));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
